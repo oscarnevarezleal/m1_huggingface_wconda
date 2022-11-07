902d9ded91d236decfbb888af1620e2c5db2ac9b
onevarez <fu.wire@gmail.com>
2022-11-07 10:35:48 -0600

    fix groundbase work and deps hell; diffusers already installs the right pytorch version.

--- start.sh
+++ start.sh
@@ -1,16 +1,9 @@
 #!/usr/bin/env bash
-
-# Rust ( in case not installed before )
+# Rust ( in case it's not installed )
 # curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 # source "$HOME/.cargo/env"
 
-CONDA_SUBDIR=osx-64 conda env create -f environment.yaml
-
-conda init bash &&
-eval "$(command conda 'shell.bash' 'hook' 2>/dev/null)" &&
+CONDA_SUBDIR=osx-arm64 conda env create -f environment.yaml
+conda init bash
 conda activate sdm1
-
-conda install -c conda-forge diffusers==0.7.2
-conda install -c conda-forge transformers accelerate
-
-#--
+CONDA_SUBDIR=osx-arm64 conda install -y ftfy spacy accelerate -c pytorch
